846e6626d685ab19eb2a2412aae431c48929f796
Amy Li <amy.al12345@gmail.com>
2023-03-03 13:05:11 -0800

    Added blue_art gatt arduino code.

--- src/App.js
+++ src/App.js
@@ -12,15 +12,32 @@ function isWebBLEAvailable(){
 function connectBluetooth(){
   if(isWebBLEAvailable()){
     let options = {
-      acceptAllDevices: true
-  }
-  console.log("requesting BLE device info...")
-  navigator.bluetooth.requestDevice(options).then(device =>{
-    device.gatt.connect()
-    console.log("Connected to " + device.name)
-  }).catch(error=> {
-      console.log("Request device error:" + error)
-  })
+      filters: [{name: 'Adafruit Bluefruit LE'}, { services: ["c48e6067-5295-48d3-8d5c-0395f61792b1"] },],
+      optionalServices: ["battery_service"],
+      // acceptAllDevices: true
+    }
+    console.log("requesting BLE device info...")
+    navigator.bluetooth.requestDevice(options).then(device =>{
+      device.gatt.connect()
+      console.log("Connected to " + device.name)
+      console.log("UUID: " + device.gatt.uuid) // seems that uuid has not been set
+      console.log("Service options: " + device.gatt.getPrimaryServices("6e400001-b5a3-f393-e0a9-e50e24dcca9e"))
+    }).then(server => {
+      // Getting Battery Service…
+      return server.getPrimaryService('battery_service');
+    })
+    .then(service => {
+      // Getting Battery Level Characteristic…
+      return service.getCharacteristic('battery_level');
+    })
+    .then(characteristic => {
+      // Reading Battery Level…
+      return characteristic.readValue();
+    })
+    .then(value => {
+      console.log(`Battery percentage is ${value.getUint8(0)}`);
+    })
+    .catch(error => { console.error(error); });
   }
 }
 
