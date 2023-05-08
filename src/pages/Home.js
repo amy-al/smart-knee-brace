2ecf458c631db0e07e93e33b5e9846ab20744fe4
Amy Li <amy.al12345@gmail.com>
2023-05-07 23:02:08 -0700

    Added routing

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+
+import React from "react";
+// importing Link from react-router-dom to navigate to 
+// different end points.
+import { Link } from "react-router-dom";
+  
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to your Smart Knee Brace!</h1>
+      <br />
+      <ul>
+        <li>
+          {/* Endpoint to route to Home component */}
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          {/* Endpoint to route to About component */}
+          <Link to="/stretch">Stretch Sensor Reading</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+  
+export default Home;
