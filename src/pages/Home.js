2cb8c158b426c66a63bb8e71bacbbd68a76ee490
Amy Li <amy.al12345@gmail.com>
2023-05-09 19:00:28 -0700

    Added dropdown menu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,37 @@
 
-import React from "react";
+import React, {useState} from "react";
 // importing Link from react-router-dom to navigate to 
 // different end points.
 import { Link } from "react-router-dom";
+
+import Select from 'react-select'
+
+
+
+const options = [
+  { value: 'acl', label: 'ACL injury' },
+  { value: 'pcl', label: 'PCL injury' },
+  { value: 'sprain', label: 'Sprain' },
+  { value: 'meniscal', label: 'Meniscal tear'},
+  { value: 'tendon', label: 'Tendon tear'},
+  { value: 'collateral', label: 'Collateral ligament injuries'},
+  { value: 'general', label: 'General knee pain'},
+  { value: 'other', label: 'Other'}
+]
+
+// const [selectedOption, setSelectedOption] = useState(null); // use the selected option and change the state
+
+const DropdownMenu = () => (
+  <Select options={options} />
+)
+
   
 const Home = () => {
   return (
     <div>
       <h1>Welcome to your Smart Knee Brace!</h1>
+      <h2>Select your knee injury:</h2>
+      <DropdownMenu/>
       <br />
       <ul>
         <li>
