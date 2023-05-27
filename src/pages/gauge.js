045cf8709835f97636f2de8316adace82778d01e
Amy Li <amy.al12345@gmail.com>
2023-05-27 12:38:09 -0700

    edited gauge

--- src/pages/gauge.js
+++ src/pages/gauge.js
@@ -1,36 +1,14 @@
-
-import '../App.css';
-import { useState } from 'react';
-// import GaugeChart from 'react-gauge-chart';
-import StretchingInterface from '../stretch.js';
-
 import React from "react"
 import { arc } from "d3-shape"
 import { scaleLinear } from "d3-scale"
 import { format } from "d3-format"
 
-
-function isWebBLEAvailable(){
-  if(!navigator.bluetooth){
-      console.log("Web Bluetooth is not available.")
-      return false
-  }
-  return true
-}
-
-function Stretch_sensor() {
-  const [sensorReading, setSensorReading] = useState(-1);
-  const readingDisplay = sensorReading !== -1 ? <p>{sensorReading}</p> : null;
-  console.log(readingDisplay)
-
-  // Gauge
-
 const Gauge = ({
-  value=sensorReading,
+  value=50,
   min=0,
   max=100,
-  label="Stretch reading",
-  units="Inches",
+  label,
+  units,
 }) => {
   const backgroundArc = arc()
     .innerRadius(0.65)
@@ -172,59 +150,3 @@ const getCoordsOnArc = (angle, offset=10) => [
   Math.cos(angle - (Math.PI / 2)) * offset,
   Math.sin(angle - (Math.PI / 2)) * offset,
 ]
-
-// end of gauge
-  
-  return (
-    <div className="App">
-      {readingDisplay}
-      <button onClick={connectBluetooth}>
-        Connect
-      </button>
-      <div>
-        < Gauge>
-        </Gauge>
-        {/* <GaugeChart id="stretch-gauge"
-          nrOfLevels={20}
-          percent={(sensorReading - 2) / 4}
-          textColor={"#123456"}
-          needleColor={"#000000"}
-        /> */}
-      </div>
-    </div>
-  );
-  
-  function handleChange(event) {
-    const reading = event.target.value.getUint16(0, true)
-    const voltage = reading / 1023.0 * 3.3
-    const resistance = (voltage * 1000) / (3.3 - voltage)
-    const length = resistance / 350.0 
-    setSensorReading(length)
-  }
-  function connectBluetooth() {
-    if (isWebBLEAvailable()){
-      let options = {
-        acceptAllDevices: true,
-        // filters: [{name: 'Smart Knee Brace'},],
-        optionalServices: [
-          "6e400001-b5a3-f393-e0a9-e50e24dcca9e",
-          "59f5bf56-803a-4525-8202-1284c6d0f073"], // 59f5bf56-803a-4525-8202-1284c6d0f073
-      }
-      console.log("requesting BLE device info...")
-      navigator.bluetooth.requestDevice(options).then(device =>{
-        return device.gatt.connect()
-      }).then(server => {
-        return server.getPrimaryServices("59f5bf56-803a-4525-8202-1284c6d0f073")
-      }).then(services => {
-        return services[0].getCharacteristic(0x0002)
-      }).then(characteristic => {
-        console.log(characteristic)
-        return characteristic.startNotifications()
-      }).then(characteristic => {
-        characteristic.addEventListener('characteristicvaluechanged', handleChange)
-      })
-    }
-  }
-}
-
-export default Stretch_sensor;
