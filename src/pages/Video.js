b5fb145cea78b0e40837f6e52608f64132034378
JacobSandler <jsandler31415@gmail.com>
2023-05-26 19:52:15 -0700

    update: added figma styles and functionality to video page

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
+import { FaHome } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 import "./video.css";
 
 const videos = [
-  { id: 1, src: "https://gfycat.com/ifr/adeptimpurecanadagoose" },
-  { id: 2, src: "https://www.youtube.com/embed/ikt6NME0k9E" },
-  { id: 3, src: "https://www.youtube.com/embed/zzoRfu1gzC0" },
-  { id: 4, src: "https://www.youtube.com/embed/bvryJd1FqyQ" }
+  { id: 1, src: "https://gfycat.com/ifr/adeptimpurecanadagoose", name: "Video 1" },
+  { id: 2, src: "https://www.youtube.com/embed/ikt6NME0k9E", name: "Video 2"  },
+  { id: 3, src: "https://www.youtube.com/embed/zzoRfu1gzC0", name: "Video 3"  },
+  { id: 4, src: "https://www.youtube.com/embed/bvryJd1FqyQ", name: "Video 4"  }
 ];
 
 const INTERVAL = 5000; // in milliseconds
@@ -16,6 +18,7 @@ export default function VidApp() {
   const [paused, setPaused] = useState(true);
   const timerRef = useRef();
 
+  /*
   let message;
   if (timer > 0 && paused)
     message = "Click button to start!";
@@ -23,6 +26,7 @@ export default function VidApp() {
     message = "Keep stretching!";
   else
     message = "Stop";
+  */
 
   useEffect(() => {
     if (!paused) {
@@ -51,30 +55,69 @@ export default function VidApp() {
   };
 
   const activeVideo = videos[currentIndex];
+  var previousVideo = videos[currentIndex - 1];
+  var nextVideo = videos[(currentIndex + 1) % videos.length];
+
+  if(currentIndex == 0){
+    previousVideo = videos[videos.length - 1];
+  }
+
 
   return (
-    <div className="container">
-    <div className="video-container">
-      <iframe
-        key={activeVideo.id}
-        title={`video-${activeVideo.id}`}
-        src={activeVideo.src}
-        frameBorder="0"
-        allowFullScreen
-      />
-    </div>
-    <div className="timer-container">
-      <div className="timer">{timer.toFixed(1)}</div>
-      <div className="tagline">{message}</div>
-      {paused ? (
-          <button className="start-timer" onClick={handleStartTimer}>
-            Start
-          </button>
-        ) : (
-          <button className="reset-timer" onClick={handleResetTimer}>
-            Reset Timer
-          </button>
-        )}
+    <div>
+      <div className="navbar">
+        <Link className="home-button"
+              tabIndex={-1}
+              to="/"
+        >
+          <FaHome />
+        </Link>
+      </div>
+
+      <div className="container">
+
+      <div className="video-top">
+        <div className="previous-video">{previousVideo.name}</div>
+        <div className="title">{activeVideo.name}</div>
+        <div className="next-video">{nextVideo.name}</div>
+      </div>
+
+      <div className="video-container">
+        <iframe
+          key={activeVideo.id}
+          title={`video-${activeVideo.id}`}
+          src={activeVideo.src}
+          frameBorder="0"
+          allowFullScreen
+        />
+      </div>
+
+      <div className="timer-container">
+        <div className="timer">{timer.toFixed(1)}</div>
+        {paused ? (
+            <button className="start-timer" onClick={handleStartTimer}>
+              Start
+            </button>
+          ) : (
+            <button className="reset-timer" onClick={handleResetTimer}>
+              Reset Timer
+            </button>
+          )}
+      </div>
+      <div className="navigation-container">
+        <button
+          className="previous-button"
+          onClick={() => setCurrentIndex((currentIndex - 1 + videos.length) % videos.length)}
+        >
+          Previous
+        </button>
+        <button
+          className="next-button"
+          onClick={() => setCurrentIndex((currentIndex + 1) % videos.length)}
+        >
+          Next
+        </button>
+      </div>
     </div>
   </div>
     );
